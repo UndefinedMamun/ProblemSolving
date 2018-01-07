52335213a07e9e372989cb71deb32303d75d4116
UndefinedMamun <bkmmamun@gmail.com>
2018-01-08 01:03:44 +0600

    solved few more

--- Raff/practicecpp2.cpp
+++ Raff/practicecpp2.cpp
@@ -8,26 +8,12 @@
 #include <algorithm>
 
 
-bool check(int ar[60],int s,int n)
-{
-    int i;
-    for(i=0;i<s;i++)
-    {
-        if(ar[i]==n)
-        {
-            return true;
-        }
-    }
-    return false;
-}
+
 using namespace std;
 int main()
 {
-    int a[60]= {4,1,1,1},b[5]= {1,1,2,2,2},i,j,r[60];
-    if(check(a,4,2))
-        cout<<"found";
-    else
-        cout<<"not found";
+    int d=0;
+    cout<<d%360;
     return 0;
 }
 
--- cpp_file_ input_output/inOut.cpp
+++ cpp_file_ input_output/inOut.cpp
@@ -1,14 +1,48 @@
 #include <fstream>
-
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    ifstream in;
-    ofstream output;
-    in.open("in.txt");
-    output.open("out.txt");
+    ifstream fin;
+    ofstream fout;
+    fin.open("in.txt");
+    fout.open("out.txt");
 
+    int n,m,i,j,b[100][100],r;
+    while(fin>>n>>m)
+    {
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+                fin>>b[i][j];
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                r=0;
+                if(b[i][j]==0)
+                {
+                    if(j!=0) // check left
+                        b[i][j-1]==1 ? r++ : r=r;
+                    if(i!=0) // check up
+                        b[i-1][j]==1 ? r++ : r=r;
+                    if(j!=m-1) // check right
+                        b[i][j+1]==1 ? r++ : r=r;
+                    if(i!=n-1) // check down
+                        b[i+1][j]==1 ? r++ : r=r;
+                    fout<<r;
+
+                }
+                else
+                    fout<<9;
+            }
+            fout<<endl;
+        }
+    }
+    return 0;
 
 }
